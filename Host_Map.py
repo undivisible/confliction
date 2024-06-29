d6b929a476359d904a790535797d05385ce8d7c9
FOX0018 <FOX0018@elthamhs.vic.edu.au>
2024-06-30 02:04:01 +1000

    Create project file structure

---
+++ Host_Map.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_map():
+    return render_template("home_pins.html")
+
+
+@app.route("/Ukraine")
+def ukraine_map():
+    return render_template("ukraine.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)  # Run on all interfaces for external access
